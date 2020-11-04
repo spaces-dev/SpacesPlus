ea7698395b698bfcb7fe8642e95ab8b9a317c604
Vitalij Ryndin <crashmax2015@mail.ru>
2020-11-04 20:04:41 +0800

    oldHeader fix!

--- src/components/oldHeader.ts
+++ src/components/oldHeader.ts
@@ -1,15 +1,23 @@
 import { qsa } from '../utils'
 
+/**
+ * ? Меняет местами кнопки почты и ленты местами (старое расположение)
+ * ! molimawka, Я был дураком, Извини! ¯\_(ツ)_/¯
+ * @param b On/Off
+ */
 export const oldHeader = (b: boolean) => {
-    qsa('a[class="horiz-menu__link"').forEach((v, k, p) => {
 
-        // Клонируем ленту
-        let elem1 = p[2].cloneNode(true)
+    // кнопки шапки
+    let p: any = qsa('a[class="horiz-menu__link"')
 
-        // Клонируем почту
-        let elem2 = p[4].cloneNode(true)
+    // Клонируем ленту
+    let tab1 = p[2].cloneNode(true)
 
-        // @ts-ignore Костылище!
-        if (b || (!b && p[2].pathname === '/mail/')) { p[4].replaceWith(elem1); p[2].replaceWith(elem2) }
-    })
+    // Клонируем почту
+    let tab2 = p[4].cloneNode(true)
+
+    if (b || p[2].pathname === '/mail/') {
+        p[4].replaceWith(tab1)
+        p[2].replaceWith(tab2)
+    }
 }
