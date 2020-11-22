25ee7fa93089784603f76d4383e2f3704ba0f028
Vitalij Ryndin <crashmax2015@mail.ru>
2020-11-22 18:55:51 +0800

    ref del readers

--- src/components/deleteReaders.ts
+++ src/components/deleteReaders.ts
@@ -14,121 +14,121 @@ import {
 
 import { SPACES, DATA } from '../strings'
 
-/**
- * TODO: Типизировать поля!
- */
 export const deleteReaders = () => {
-    let buttons = qs('#SP_PLUS_BUTTONS_R')
+    try {
+        let buttons = qs('#SP_PLUS_BUTTONS_R'),
+            delLinks = qsa(`a[href^="${SPACES}/lenta/reader_delete/"`)
 
-    if (getPath() === '/lenta/readers/' && !buttons) {
-        try {
-            let links = qsa(`a[href^="${SPACES}/lenta/reader_delete/"`)
+        if (getPath() === '/lenta/readers/' && delLinks && !buttons) {
 
-            if (links && !buttons) {
-                let checkboxArr: Element[] = []
+            let checkboxArr: Element[] = []
 
-                for (let link of links) {
-                    link.style.textAlign = 'center'
+            for (let link of delLinks) {
 
-                    let chWrap = ce('label', { class: 'stnd-link icon-link sp-ch-readers' })
+                let chWrap = ce('label', { class: 'stnd-link icon-link sp-ch-readers' })
 
-                    let userId = `SP_DR_${getParams((<HTMLLinkElement>link).href)['user']}`
+                let userId = getParams((<HTMLLinkElement>link).href)['user']
 
-                    let bChbx = ce('input', {
-                        type: 'checkbox',
-                        class: 'sp-cbfr sp-checkbox-square',
-                        id: userId
-                    })
+                let bChbx = ce('input', {
+                    type: 'checkbox',
+                    class: 'sp-cbfr sp-checkbox-square',
+                    id: userId
+                })
 
-                    let ckbxlb = ce('label', {
-                        style: 'margin-left: 0px',
-                        attr: { 'for': userId }
-                    })
+                let ckbxlb = ce('label', {
+                    style: 'margin-left: 0px',
+                    attr: { 'for': userId }
+                })
 
-                    chWrap.appendChild(bChbx)
-                    chWrap.appendChild(ckbxlb)
-                    link.after(chWrap)
-                    checkboxArr.push(bChbx)
-                }
+                chWrap.appendChild(bChbx)
+                chWrap.appendChild(ckbxlb)
+                link.after(chWrap)
+                checkboxArr.push(bChbx)
+            }
 
-                let lastParent = links[links.length - 1]?.parentNode?.parentNode
+            let buttonsDiv = ce('div', {
+                class: 'user__tools_last',
+                id: 'SP_PLUS_BUTTONS_R'
+            })
 
-                if (lastParent) {
-                    let buttonsDiv = ce('div', {
-                        class: 'user__tools_last',
-                        id: 'SP_PLUS_BUTTONS_R'
-                    })
+            const chooseAllButton = ce('button', {
+                style: 'border-right: unset',
+                class: 'user__tools-link table__cell sp_btn-list',
+                html: '<span class="sp sp-ok-blue"></span><span class="sp-ch-text">Выбрать все</span>',
+                onclick: (e: MouseEvent) => {
+                    if (e.target instanceof Element) {
+                        let parent = e.target.nodeName === 'SPAN' ?
+                            <Element>e.target.parentNode :
+                            <Element>e.target
 
-                    const chooseAllButton = ce('button', {
-                        style: 'border-right: unset',
-                        class: 'user__tools-link table__cell sp_btn-list',
-                        html: '<span class="sp sp-ok-blue"></span><span class="sp-ch-text">Выбрать все</span>',
-                        onclick: (e: any) => {
-                            let parent = e.target.nodeName === 'SPAN' ? e.target.parentNode : e.target
-
-                            for (let ch of checkboxArr) {
-                                (<HTMLInputElement>ch).checked = parent.innerHTML.indexOf('Выбрать все') !== -1 ? true : false
-                            }
-
-                            parent.innerHTML = `<span class="sp sp-ok-blue"></span><span class="sp-ch-text">${parent.innerHTML.indexOf('Выбрать все') !== -1 ? 'Снять отметки' : 'Выбрать все'}</span>`
-                            return false
+                        for (let ch of checkboxArr) {
+                            (<HTMLInputElement>ch).checked =
+                                parent.innerHTML.indexOf('Выбрать все') !== -1 ?
+                                    true :
+                                    false
                         }
-                    })
 
-                    const deleteReadersButton = ce('button', {
-                        class: 'user__tools-link table__cell sp_btn_line sp_btn-list',
-                        html: '<span class="sp sp-remove-red"></span><span class="sp-del-text">Удалить выбранных</span>',
-                        onclick: () => {
-                            let count: number = 0,
-                                readers: string[] = []
-
-                            for (let ch of checkboxArr) {
-                                if ((<HTMLInputElement>ch).checked) {
-                                    readers.push(/^SP_DR_([A-Za-z0-9\_]+)$/i.exec(ch.id)![1])
-                                    count++
-                                }
-                            }
-
-                            if (count > 0) {
-
-                                confirmBox(`Вы действительно хотите удалить ${count} ${declStr(count)}?`, true, async () => {
-
-                                    let allCount = count
-
-                                    for (let reader of readers) {
-                                        messageBox(`Осталось удалить ${count--} из ${allCount} ${declStr(count)}`, 'Подождите немного... <span style="padding-right: 10px" class="ico ico_spinner"></span>', false)
-
-                                        await http('POST', `${SPACES}/lenta/reader_delete/?user=${reader}`, false, `&CK=${DATA.CK}&cfms=Удалить`).then(e => {
-                                            info('Удалили читателя', e)
-                                        })
-                                    }
-
-                                    document.location.reload()
-                                })
-                            } else {
-                                messageBox('Внимание!', 'Отметьте галочкой, теx читателей, которых вы хотите удалить и попробуйте еще раз', true, 5)
-                            }
-                            return false
-                        }
-                    })
-
-                    buttonsDiv.appendChild(deleteReadersButton)
-                    buttonsDiv.appendChild(chooseAllButton)
-
-                    let main = qs('#main'),
-                        pgn = qs('div.pgn-wrapper')
-
-                    if (pgn) {
-                        pgn.prepend(buttonsDiv)
-                    } else if (main) {
-                        buttonsDiv.classList.add('widgets-group')
-                        main.prepend(buttonsDiv)
+                        parent.innerHTML = `
+                            <span class="sp sp-ok-blue"></span>
+                            <span class="sp-ch-text">
+                                ${parent.innerHTML.indexOf('Выбрать все') !== -1 ?
+                                'Снять отметки' :
+                                'Выбрать все'}
+                            </span>
+                        `
                     }
                 }
+            })
+
+            const deleteReadersButton = ce('button', {
+                class: 'user__tools-link table__cell sp_btn_line sp_btn-list',
+                html: '<span class="sp sp-remove-red"></span><span class="sp-del-text">Удалить выбранных</span>',
+                onclick: () => {
+                    let readers: string[] = []
+
+                    for (let ch of checkboxArr) {
+                        if ((<HTMLInputElement>ch).checked) readers.push(ch.id)
+                    }
+
+                    let count: number = readers.length
+
+                    if (count > 0) {
+
+                        confirmBox(`Вы действительно хотите удалить ${count} ${declStr(count)}?`, true, async () => {
+
+                            let allReaders = count
+
+                            for (let reader of readers) {
+                                messageBox(`Осталось удалить ${count--} из ${allReaders} ${declStr(count)}`, 'Подождите немного... <span style="padding-right: 10px" class="ico ico_spinner"></span>', false)
+
+                                await http('POST', `${SPACES}/lenta/reader_delete/?user=${reader}`, false, `&CK=${DATA.CK}&cfms=Удалить`).then(e => {
+                                    info('Удалили читателя', e)
+                                })
+                            }
+
+                            document.location.reload()
+                        })
+                    } else {
+                        messageBox('Внимание!', 'Отметьте галочкой, теx читателей, которых вы хотите удалить и попробуйте еще раз', true, 5)
+                    }
+                }
+            })
+
+            buttonsDiv.appendChild(deleteReadersButton)
+            buttonsDiv.appendChild(chooseAllButton)
+
+            let main = qs('#main'),
+                pgn = qs('div.pgn-wrapper')
+
+            if (pgn) {
+                pgn.prepend(buttonsDiv)
+            } else if (main) {
+                buttonsDiv.classList.add('widgets-group')
+                main.prepend(buttonsDiv)
             }
-        } catch (e) {
-            error('deleteReaders.ts', e)
         }
+    } catch (e) {
+        error('deleteReaders.ts', e)
     }
 }
 
