b80455c02c609f2e8978f73682badfdead0c21e7
Vitalij Ryndin <crashmax2015@mail.ru>
2020-11-23 02:03:21 +0800

    description & promise onload

--- src/components/qrCode.ts
+++ src/components/qrCode.ts
@@ -6,7 +6,7 @@ import {
     messageBox
 } from '../utils'
 
-import { DATA } from '../strings'
+import { DATA, HTTP } from '../strings'
 
 export const qrCode = () => {
     try {
@@ -25,9 +25,27 @@ export const qrCode = () => {
                 onclick: () => {
                     messageBox(
                         'Сканируйте QR-код c телефона',
-                        `<img src="https://chart.googleapis.com/chart?cht=qr&chs=256x256&chl=${DATA.SID}" class="sp_img-center"></img>`,
+                        `<div id="SP_QR_CODE"><img src="${HTTP}//spac.me/i/preloader.gif" class="sp_img-center" id="SP_QR_LOADER"></div>`,
                         true
                     )
+
+                    // https://developers.google.com/chart/infographics/docs/qr_codes
+                    let qr = qrCodeLoader(`https://chart.googleapis.com/chart?cht=qr&chs=256x256&chl=${DATA.SID}`),
+                        loader = qs('#SP_QR_LOADER'),
+                        target = qs('#SP_QR_CODE')
+
+                    qr.then(e => {
+                        loader.remove()
+                        target.appendChild(e)
+
+                        target.appendChild(ce('ol', {
+                            html: `
+                                <li>Запустите <b>OpenSpaces</b> на телефоне</li>
+                                <li>На странице авторизации нажмите «<b>Login by QR code</b>»</li>
+                                <li>Сканируйте этот код для авторизации</li>
+                            `
+                        }))
+                    })
 
                     return false
                 }
@@ -38,4 +56,14 @@ export const qrCode = () => {
     } catch (e) {
         error('qrCode.ts', e)
     }
+}
+
+const qrCodeLoader = (url: string): Promise<Element> => {
+    return new Promise((resolve, reject) => {
+        const image = new Image()
+        image.src = url
+        image.className = 'sp_img-center'
+        image.onload = () => resolve(image)
+        image.onerror = () => reject(new Error('loading error'))
+    })
 }
