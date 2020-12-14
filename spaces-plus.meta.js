7aa4696409a2e738c18942329aecf47cdd87e8fb
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-12-14 10:21:45 +0000

    Deploy spaces-dev/SpacesPlus to github.com/spaces-dev/SpacesPlus.git:gh-pages

--- spaces-plus.meta.js
+++ spaces-plus.meta.js
@@ -4,10 +4,8 @@
 // @author      Vitalij Ryndin
 // @description 🚀 Powerful userscript for Spaces.ru
 // @homepage    https://spaces-dev.github.io/SpacesPlus
-// @match       *://(spaces.ru|spac.me|spcs.me|spaces.im|gdespaces.com|spac1.com|spac1.net|spaces-blogs.com)/*
-// @namespace   https://spaces-dev.github.io/SpacesPlus
 // @icon        https://spaces-dev.github.io/SpacesPlus/icons/logo_96.png
-// @include     /^(http|https)://(spaces.ru|spac.me|spcs.me|spaces.im|gdespaces.com|spac1.com|spac1.net|spaces-blogs.com).*$/
+// @include     /^(http|https)://(spaces.ru|spcs.me|spaces.im|gdespaces.com|spac1.com|spac1.net|spaces-blogs.com).*$/
 // @homepageURL https://github.com/spaces-dev/SpacesPlus
 // @require     https://spaces-dev.github.io/SpacesPlus/libs/colorpicker.js
 // @grant       none
