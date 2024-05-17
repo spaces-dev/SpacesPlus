66ad87f4601a7779b4dd3b35d9cfff1da6c76af0
Vitalij Ryndin <crashmax.dev@gmail.com>
2024-05-17 08:22:39 +0800

    fix: iife

--- vite.config.ts
+++ vite.config.ts
@@ -36,6 +36,9 @@ export default defineConfig((config) => {
         },
         server: {
           port: 3000
+        },
+        esbuildTransformOptions: {
+          banner: ';'
         }
       })
 
